4fe0de75100632c5b497268e312ccfd843c9900e
liaoguoguang <liaoguoguang@yy.com>
2023-11-26 11:36:16 +0800

    11

--- sdk/src/main/java/com/github/movins/event/BlockBase.java
+++ sdk/src/main/java/com/github/movins/event/BlockBase.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class BlockBase extends Dispatcher implements IBlock {
     private Object mModule;
     private boolean mStoped;
+
     private Map<String, Method> mHandles= new HashMap<>();
 
     @Override
